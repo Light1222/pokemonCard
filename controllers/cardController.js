8a42cb9d67fe4d91a91efc40b1905c9942c71ef2
light1222 <r7qb@fresnou.org>
2022-12-16 00:39:04 -0800

    Home commit

---
+++ controllers/cardController.js
@@ -0,0 +1,45 @@
+module.exports.viewAll = function(req,res,) {
+    const pokemons =[ {
+        id: 1,
+        name: req.body.name,
+        hp: req.body.hp,
+        type: req.body.type,
+        poke_img: req.body.poke_img,
+        mtype1: req.body.mtype1,
+        mtype2: req.body.mtype2,
+        mtype3: req.body.mtype3,
+        mname: req.body.mname,
+        d1: req.body.d1,
+        m2type1: req.body.m2type1,
+        m2type2: req.body.m2type2,
+        m2type3: req.body.m2type3,
+        m2name: req.body.m2name,
+        d2: req.body.d1,
+        wentype: req.body.wentype,
+        bentype: req.body.bentype,
+        rentype: req.body.rentype
+    },
+    {
+        id: 2,
+        name: req.body.name,
+        hp: req.body.hp,
+        type: req.body.type,
+        poke_img: req.body.poke_img,
+        mtype1: req.body.mtype1,
+        mtype2: req.body.mtype2,
+        mtype3: req.body.mtype3,
+        mname: req.body.mname,
+        d1: req.body.d1,
+        m2type1: req.body.m2type1,
+        m2type2: req.body.m2type2,
+        m2type3: req.body.m2type3,
+        m2name: req.body.m2name,
+        d2: req.body.d1,
+        wentype: req.body.wentype,
+        bentype: req.body.bentype,
+        rentype: req.body.rentype
+        }];
+    res.render('index', {pokemon})
+}
+
+const cardController = require('../controllers'/cardController);
--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', cardController.viewAll);
 
-router.post('/card', function(req, res, ){
-  res.render('card', {
-    name: req.body.name,
-    hp: req.body.hp,
-    type: req.body.type,
-    poke_img: req.body.poke_img,
-    mtype1: req.body.mtype1,
-    mtype2: req.body.mtype2,
-    mtype3: req.body.mtype3,
-    mname: req.body.mname,
-    d1: req.body.d1,
-    m2type1: req.body.m2type1,
-    m2type2: req.body.m2type2,
-    m2type3: req.body.m2type3,
-    m2name: req.body.m2name,
-    d2: req.body.d1,
-    wentype: req.body.wentype,
-    bentype: req.body.bentype,
-    rentype: req.body.rentype
-  });
-})
+// router.post('/card', function(req, res, ){
+//   res.render('card', {
 
 module.exports = router;
 
