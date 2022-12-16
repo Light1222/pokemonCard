e5eeb87ca342bf70d6ff234dfc4c915bede6725b
light1222 <r7qb@fresnou.org>
2022-12-16 02:51:49 -0800

    commit

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,56 +1,163 @@
 const {pokemon} = require('../models')
-module.exports.viewAll = async function(req, res,) {
-    const pokemons = await pokemon.findAll();
-    res.render('index', {pokemons});
+const types = ['Bug', 'Electric', 'Fairy', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water']
+
+module.exports.viewAll = async function(req, res, next){
+    let findTypes = ['All'];
+    for(let i = 0; i < types.length; i++) {
+        findTypes.push(types[i]);
+    }
+    let pokemons;
+    let findType = req.query.type || all
+    let findRandom = req.query.random || false;
+    if (findType === 'All') {
+        pokemons = await pokemon.findAll
+    } else {
+        pokemons = await pokemon.findAll({
+            where: {
+                type: findType
+            }
+        });
+    }
+    if (pokemons.length > 0 && findRandom){
+        let randomIndex = getRandomInt(pokemons.length)
+        pokemons = [pokemons[randomIndex]]
+    }
+    res.render('index', {pokemons, types:findTypes, findType, findRandom} )
+}
+
+module.exports.updateCard = async function(req, res, next) {
+    const pokemon = await card.findByPk(
+        req.params.id
+    )
+    res.render('edit', {card, types});
+}
+
+
+// module.exports.viewAll = async function(req, res, next) {
+//     const pokemons = await pokemon.findAll();
+//     res.render('index', {pokemons});
+// }
+
+
+
+
+
+
+module.exports.updateCard = async function(req,res) {
+    await pokemon.update(
+        {
+        name: req.body.name,
+        hp: req.body.hp,
+        type: req.body.type,
+        poke_img: req.body.poke_img,
+        mtype1: req.body.mtype1,
+        mtype2: req.body.mtype2,
+        mtype3: req.body.mtype3,
+        mname: req.body.mname,
+        d1: req.body.d1,
+        m2type1: req.body.m2type1,
+        m2type2: req.body.m2type2,
+        m2type3: req.body.m2type3,
+        m2name: req.body.m2name,
+        d2: req.body.d1,
+        wentype: req.body.wentype,
+        bentype: req.body.bentype,
+        rentype: req.body.rentype,
+    },
+    {
+        where:
+            {
+                id: req.params.id
+            }
+    })
+res.redirect('/');
+}
+
+module.exports.deleteCard = async function(req,res) {
+    await Card.destroy(
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        })
+    res.redirect('/')
+}
+
+module.exports.renderAddForm = async function(req,res) {
+    const pokemon = {
+        name: "",
+        hp: "",
+        type: types[0],
+        poke_img: "",
+        mtype1: "",
+        mtype2: "",
+        mtype3: "",
+        mname: "",
+        d1: "",
+        m2type1: "",
+        m2type2: "",
+        m2type3: "",
+        m2name: "",
+        d2: "",
+        wentype: "",
+        bentype: "",
+        rentype: ""
+    }
+    res.render('add', {pokemon, types})
+}
+
+module.exports.addCard = async function(req, res) {
+    await pokemon.create(
+        {
+            name: req.body.name,
+            hp: req.body.hp,
+            type: req.body.type,
+            poke_img: req.body.poke_img,
+            mtype1: req.body.mtype1,
+            mtype2: req.body.mtype2,
+            mtype3: req.body.mtype3,
+            mname: req.body.mname,
+            d1: req.body.d1,
+            m2type1: req.body.m2type1,
+            m2type2: req.body.m2type2,
+            m2type3: req.body.m2type3,
+            m2name: req.body.m2name,
+            d2: req.body.d1,
+            wentype: req.body.wentype,
+            bentype: req.body.bentype,
+            rentype: req.body.rentype
+        })
+    res.redirect('/')
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
 }
 
 
 
 
-
-
-// module.exports.viewAll = function(req,res,) {
-//     const pokemons =[ {
-//         id: 1,
-//         name: req.body.name,
-//         hp: req.body.hp,
-//         type: req.body.type,
-//         poke_img: req.body.poke_img,
-//         mtype1: req.body.mtype1,
-//         mtype2: req.body.mtype2,
-//         mtype3: req.body.mtype3,
-//         mname: req.body.mname,
-//         d1: req.body.d1,
-//         m2type1: req.body.m2type1,
-//         m2type2: req.body.m2type2,
-//         m2type3: req.body.m2type3,
-//         m2name: req.body.m2name,
-//         d2: req.body.d1,
-//         wentype: req.body.wentype,
-//         bentype: req.body.bentype,
-//         rentype: req.body.rentype
-//     },
-//     {
-//         id: 2,
-//         name: req.body.name,
-//         hp: req.body.hp,
-//         type: req.body.type,
-//         poke_img: req.body.poke_img,
-//         mtype1: req.body.mtype1,
-//         mtype2: req.body.mtype2,
-//         mtype3: req.body.mtype3,
-//         mname: req.body.mname,
-//         d1: req.body.d1,
-//         m2type1: req.body.m2type1,
-//         m2type2: req.body.m2type2,
-//         m2type3: req.body.m2type3,
-//         m2name: req.body.m2name,
-//         d2: req.body.d1,
-//         wentype: req.body.wentype,
-//         bentype: req.body.bentype,
-//         rentype: req.body.rentype
-//         }];
+        // id: 2,
+        // name: req.body.name,
+        // hp: req.body.hp,
+        // type: req.body.type,
+        // poke_img: req.body.poke_img,
+        // mtype1: req.body.mtype1,
+        // mtype2: req.body.mtype2,
+        // mtype3: req.body.mtype3,
+        // mname: req.body.mname,
+        // d1: req.body.d1,
+        // m2type1: req.body.m2type1,
+        // m2type2: req.body.m2type2,
+        // m2type3: req.body.m2type3,
+        // m2name: req.body.m2name,
+        // d2: req.body.d1,
+        // wentype: req.body.wentype,
+        // bentype: req.body.bentype,
+        // rentype: req.body.rentype
+        // }];
 //     res.render('index', {pokemon})
 // }
 
-const cardController = require('../controllers'/cardController);
+// const cardController = require('../controllers'/cardController);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const cardController = require('../controllers/cardController');
+
 
 /* GET home page. */
 router.get('/', cardController.viewAll);
 
-// router.post('/card', function(req, res, ){
-//   res.render('card', {
+router.get('/', cardController.renderEditForm);
+
+router.get('/', cardController.updateCard);
+
+router.get('/', cardController.deleteCard);
+
+router.get('/', cardController.renderAddForm);
+
+router.get('/', cardController.addCard);
 
 module.exports = router;
 
